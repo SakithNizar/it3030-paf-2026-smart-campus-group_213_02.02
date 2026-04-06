160a0557c6388a061fb052c01f59d646409ec52b
SakithNizar <sakithmohamed54@gmail.com>
2026-04-06 11:29:17 +0530

    Delete unwanted files

--- backend/model/ResourceStatus.java
+++
@@ -1,7 +0,0 @@
-package com.group213.backend.model;
-
-public enum ResourceStatus {
-    ACTIVE,
-    OUT_OF_SERVICE,
-    UNDER_MAINTENANCE
-}
--- backend/model/ResourceType.java
+++
@@ -1,7 +0,0 @@
-package com.group213.backend.model;
-
-public enum ResourceType {
-    ROOM,
-    LAB,
-    EQUIPMENT
-}
