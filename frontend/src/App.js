9439e46c5e5793a3a507980e1491cebcccd03d57
SakithNizar <sakithmohamed54@gmail.com>
2026-04-18 16:57:07 +0530

    fix(resourse): add missed imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import "./App.css";
-// import ResourceList from "./pages/ResourceList"; // 1. Commented out the import
+import ResourceList from "./pages/ResourceList";
 import BookingManager from './components/BookingManager';
 
 function App() {
   return (
     <div className="container">
       <h1>🏫 Smart Campus Resource Management</h1>
-      
-      {/* 2. Muted teammate's component to prevent Network Errors */}
-      {/* <ResourceList /> */} 
-
+      <ResourceList />
       <BookingManager />
     </div>
   );
